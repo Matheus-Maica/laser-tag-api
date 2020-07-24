f314168cfb0f49812fb9fdb45eb4f695326a4211
Guilherme Inkotte <gui.inkotte@hotmail.com>
2020-07-24 11:26:29 -0300

    Implementing socket.io

--- index.js
+++ index.js
@@ -14,6 +14,39 @@ require('./controllers/')(app);
 
 let port = 3030;
 
-app.listen(port, () => {
+var server = app.listen(port, () => {
     console.log('Servidor rodando na porta ' + port + '!!!');
 });
+
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    console.log("New connection", socket.id)
+    var onGoingMatch;
+    socket.on('startMatch', matchInfo => {
+        onGoingMatch = true;
+        console.log("A partida começou")
+        io.sockets.emit('startMatch', "A partida começou!");
+    })
+
+    socket.on('endMatch', matchInfo => {
+        onGoingMatch = false;
+        console.log("A partida terminou")
+        io.sockets.emit('endMatch', "A partida acabou!");
+    })
+
+    socket.on('death', deathInfo => {
+        // deathinfo = {
+        //     killed: {
+        //         name,
+        //         team
+        //     },
+        //     killer: {
+        //         name,
+        //         team
+        //     },
+        //     timeStamp,
+        // }
+        io.sockets.emit('killfeed', deathInfo)
+    })
+})
