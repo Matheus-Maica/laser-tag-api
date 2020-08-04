9bb4e0877c695a2a5333393f8289e54960b361e0
30111971 <matheusmaica@gmail.com>
2020-08-04 15:01:07 -0300

    commitando diferenças

--- controllers/matchController.js
+++ controllers/matchController.js
@@ -22,7 +22,7 @@ router.get('/death/:killed/:killer', async (req, res) => {
     const id_killer = parseInt(query.killer)
     const killed = {
         player: id_killed,
-        team: id === 1 ? "blue" : "red"
+        team: id_killed === 1 ? "blue" : "red"
     }
     const killer = {
         player: id_killer === 1 ? 2 : 1,
